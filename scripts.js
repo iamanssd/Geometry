0707f13b7cdb1d923841d05e0d5c4190521e8fc3
githubsamuelkang <email.samuelkang@gmail.com>
2018-06-09 23:56:08 -0400

    edits in the index.html, and made an edit on the design of the riddle

--- scripts.js
+++ scripts.js
@@ -6,19 +6,3 @@ function openCompass(){
 
 
 }
-
-//window.onload = function() {
-//    if (window.jQuery) {  
-//        // jQuery is loaded  
-//        alert("Yeah!");
- //   } else {
-  //      // jQuery is not loaded
-   //     alert("Doesn't Work");
-   // }
-//}
-
-
-function colorChooser(){
-	var x = document.getElementById("myColor").value;
-	document.getElementById("color").innerHTML = x;
-}
--- scriptsforhomepage.js
+++ scriptsforhomepage.js
@@ -1,16 +1,3 @@
 $(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fading").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(100,0);}
-      }
-    });
-  }).scroll(); //invoke scroll-handler on page-load
+    $('.fading').fadeIn(2500);
 });
