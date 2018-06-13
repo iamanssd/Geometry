e49aa143f0e0861153e6321d55bab7e633957d4c
githubsamuelkang <email.samuelkang@gmail.com>
2018-06-12 23:25:43 -0400

    Final push

--- scripts.js
+++ scripts.js
@@ -16,10 +16,10 @@ function hide(){
 }
 
 function solutionEnter(){
-	for(i = 0; i<25; i++){
+	for(i = 0; i<29; i++){
 		document.getElementById("b-"+((i+1).toString())).innerHTML = "_" + " <br> " + ((i+1).toString()) ;
 	}
-	for(i = 0; i<Math.min(25,document.getElementById("input-sam").value.length); i++){
+	for(i = 0; i<Math.min(29,document.getElementById("input-sam").value.length); i++){
 
 		document.getElementById("b-"+((i+1).toString())).innerHTML = document.getElementById("input-sam").value.charAt(i) + " <br> " + ((i+1).toString()) ;
 	}
@@ -28,10 +28,13 @@ function solutionEnter(){
 		$("#hidden-text").show();
 
 		document.getElementById("hidden-text").textContent="You did it! Did you find the joke funny?";
-	}else if(document.getElementById("input-sam").value.toLocaleLowerCase().localeCompare("yes")!=0 && document.getElementById("input-sam").value.length>=25){
+	}else if(document.getElementById("input-sam").value.toLocaleLowerCase().localeCompare("yes")!=0 && document.getElementById("input-sam").value.length==29){
 		$("#hidden-text").show();
 
 		document.getElementById("hidden-text").textContent="Nope, try again.";
+	}
+	else{
+		$("#hidden-text").hide();
 	}
 }
 
